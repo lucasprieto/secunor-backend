b72a68d1fa88ae21089269ee7d3479243bc7b994
Lucas Prieto <lucasprieto12@gmail.com>
2022-10-09 14:17:37 -0300

    added express

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,32 @@
-import { NODE_ENV } from '@config'
+import { NODE_ENV, PORT } from '@config'
+import express from 'express'
 
 class App {
+  public app: express.Application
   public env: string
+  public port: string | number
 
   constructor() {
-    this.env = NODE_ENV
+    this.app = express()
+    this.env = NODE_ENV || 'development'
+    this.port = PORT || 3000
+
+    this.initializeMiddlewares()
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(`app listening on port ${this.port}`)
+    })
+  }
+
+  public initializeMiddlewares() {
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({ extended: true }))
+  }
+
+  public getServer() {
+    return this.app
   }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
 import App from '@/app'
 
 const app = new App()
+
+app.listen()
